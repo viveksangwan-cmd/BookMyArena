8eccd0148e9493cd67e2ea625fc3da9d35cea901
viveksangwan-cmd <viveksangwan5791@gmail.com>
2023-04-26 12:41:36 +0530

    Initialized Project

---
+++ Backend/src/main/java/com/bookmyarena/Backend/BackedForBookMyArenaApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyarena.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackedForBookMyArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackedForBookMyArenaApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/bookmyarena/Backend/BackedForBookMyArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyarena.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackedForBookMyArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
